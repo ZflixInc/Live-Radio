e2afb38233729ac7a70d51567106586f8419f5c5
ZflixInc <165347084+ZflixInc@users.noreply.github.com>
2024-06-16 08:44:20 +0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Show the splash screen for 3 seconds
-    setTimeout(function() {
-        document.getElementById("splashScreen").style.display = 'none';
-        document.getElementById("mainContent").classList.remove('hidden');
-        document.getElementById("homePage").src = "https://zawaka.site/"; // Load the website
-    }, 3000); // 3000 ms = 3 seconds
-});
